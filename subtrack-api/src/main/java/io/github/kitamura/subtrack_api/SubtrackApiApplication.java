93cf2441cd4484cf00ec418556e6eef1d01a0fdf
Seigo Kitamura <k5tar4biz@gmail.com>
2025-10-23 16:19:57 +0900

    Initial commit for project genration

---
+++ subtrack-api/src/main/java/io/github/kitamura/subtrack_api/SubtrackApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.kitamura.subtrack_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubtrackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubtrackApiApplication.class, args);
+	}
+
+}
